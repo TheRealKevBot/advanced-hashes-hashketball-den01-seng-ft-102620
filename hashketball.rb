2260c5acf3efdc0f20695d943ba3c6254cbed3f6
TheRealKevBot <Kglas001@gmail.com>
2020-10-28 13:19:33 -0600

    commit tree 236aa878b79cd46aaf77afabf81646f210404558

--- hashketball.rb
+++ hashketball.rb
@@ -127,93 +127,154 @@ def game_hash
   }
 end
 
-def num_points_scored(player_name)
-  game_hash.each do |local, data|
-    data.each do |team_data, players_data|
-      if team_data == :players
-        players_data.each do |player_stats|
-          if player_stats[:player_name] == player_name
-            return player_stats[:points]
-          end
-        end
-      end
-    end
+#
+def players
+  game_hash[:home][:players].concat game_hash[:away][:players]
+end
+#
+
+#
+def find_player_by_name (name) 
+  players.find do |player|
+    name == player[:player_name]
   end
+end
+#
+
+def num_points_scored (player_name)
+  find_player_by_name(player_name)[:points]
 end
 
-def shoe_size(player_name)
-  game_hash.each do |local, data|
-    data.each do |team_data, players_data|
-      if team_data == :players
-        players_data.each do |player_stats|
-          if player_stats[:player_name] == player_name
-            return player_stats[:shoe]
-          end
-        end
-      end
-    end
-  end
+#def num_points_scored(player_name)
+#  game_hash.each do |local, data|
+#    data.each do |team_data, players_data|
+#      if team_data == :players
+#        players_data.each do |player_stats|
+#          if player_stats[:player_name] == player_name
+#            return player_stats[:points]
+#          end
+#        end
+#      end
+#    end
+#  end
+#end
+
+def shoe_size (player)
+  find_player_by_name(player)[:shoe]
 end
 
-def team_colors(team_name)
-  game_hash.each do |local, data|
-    if data[:team_name] == team_name
-      return data[:colors]
-    end
+#def shoe_size(player_name)
+#  game_hash.each do |local, data|
+#    data.each do |team_data, players_data|
+#      if team_data == :players
+#        players_data.each do |player_stats|
+#          if player_stats[:player_name] == player_name
+#            return player_stats[:shoe]
+#          end
+#        end
+#      end
+#    end
+#  end
+#end
+
+#
+def teams (name)
+  game_hash.values.find do |team|
+    team[:team_name] == name
   end
+end
+#
+
+def team_colors (team)
+  teams(team)[:colors]
 end
 
-def team_names
-  game_hash.each_with_object([]) do |(local, data), teams|
-    teams << data[:team_name]
-  end
+#def team_colors(team_name)
+#  game_hash.each do |local, data|
+#    if data[:team_name] == team_name
+#      return data[:colors]
+#    end
+#  end
+#end
+
+def team_names 
+  [game_hash[:home][:team_name], game_hash[:away][:team_name]] 
 end
+
+#def team_names
+#  game_hash.values.each_with_object([]) do |(team), teams_names|
+#    teams_names << team[:team_name] 
+#  end
+#end
+ 
+#def team_names
+#  game_hash.each_with_object([]) do |(local, data), teams|
+#    teams << data[:team_name]
+#  end
+#end
 
 def player_numbers(team_name)
-  game_hash.each_with_object([]) do |(local, data), numbers|
-    data[:players].each do |player_data| 
-      if data[:team_name] == team_name
-        numbers << player_data[:number]
-      end
-    end
-  end
+
 end
+
+#def player_numbers(team_name)
+#  game_hash.each_with_object([]) do |(local, data), numbers|
+#    data[:players].each do |player_data| 
+#      if data[:team_name] == team_name
+#        numbers << player_data[:number]
+#      end
+#    end
+#  end
+#end
 
 def player_stats(player_name)
-  game_hash.each do |local, data|
-    data.each do |team_data, players_data|
-      if team_data == :players
-        players_data.each do |player_stats|
-          if player_stats[:player_name] == player_name
-            return player_stats
-          end
-        end
-      end
-    end
-  end
+  find_player_by_name(player_name)
 end
 
-#
+#def player_stats(player_name)
+#  game_hash.each do |local, data|
+#    data.each do |team_data, players_data|
+#      if team_data == :players
+#        players_data.each do |player_stats|
+#          if player_stats[:player_name] == player_name
+#            return player_stats
+#          end
+#        end
+#     end
+#    end
+#  end
+#end
+
+#############
+
 def biggest_shoe
-  game_hash.each_with_object([]) do |(local, data), shoes|
-    data[:players].each do |(player_data)|
-      shoes << player_data[:shoe]
-    end
-    return shoes.max
-  end
+  
 end
-#
+
+#def biggest_shoe
+#  game_hash.each_with_object([]) do |(local, data), shoes|
+#    data[:players].each do |(player_data)|
+#      shoes << player_data[:shoe]
+#    end
+#    return shoes.max
+#  end
+#end
+#############
 
 def big_shoe_rebounds
-  #biggest_shoe
-  game_hash.each do |(local, data)|
-    data[:players].each do |(player_data)|
-      if player_data[:shoe] == biggest_shoe
-        return player_data[:rebounds]
-      end
-    end
-  end
+
 end
+
+#def big_shoe_rebounds
+#  #biggest_shoe
+#  game_hash.each do |(local, data)|
+#    data[:players].each do |(player_data)|
+#      if player_data[:shoe] == biggest_shoe
+#        return player_data[:rebounds]
+#      end
+#    end
+#  end
+#end
 
 ############################_BONUS_ROUND_##########################################################################
 
